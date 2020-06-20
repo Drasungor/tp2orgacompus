c17f4cbbb106c1f075530e04ab5dcdba19cd4566
Demn <aguscambiano@gmail.com>
2020-06-20 01:15:13 -0300

    Terminamos de implementar primitivas y compila, a dormir

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdbool.h>
 #include "constants.h"
 //#define CACHE_SIZE 4096
 
@@ -6,6 +7,7 @@ typedef struct line {
   bool is_valid; //Esto representa el valid bit
   unsigned int tag; //nos sobran 2 bits porque el tag es de 6 bits
   char block[BLOCK_SIZE];
+  unsigned int times_used;
 } line_t;
 
 typedef struct set {
@@ -14,55 +16,116 @@ typedef struct set {
 
 set_t cache[NUMBER_OF_SETS];
 char memory[MEMORY_SIZE];
-unsigned int miss_rate;
+unsigned int cache_accesses;
+unsigned int number_of_misses;
+
+
+//Retorna el numero de bloque en memoria al que pertenece el address recibido
+unsigned int _get_block_number(unsigned int address) {
+  //Estas dos lineas consiguen el numero de bloque (separando la memoria en
+    //bloques de 128 bytes) al que pertenece el address
+    unsigned int address_offset = address % BLOCK_SIZE;
+    return (address - address_offset) / BLOCK_SIZE;
+}
+
+unsigned int _get_tag(unsigned int address) {
+  unsigned int block_number = _get_block_number(address);
+
+  //Estas dos lineas se fijan si el numero de bloque esta alineado a 8 bloques,
+  //si no lo estan entonces le resta el valor necesario para que lo sea
+  unsigned int block_offset = block_number % NUMBER_OF_SETS;
+  unsigned int centered_block_number = block_number - block_offset;
+
+  //Una vez alineado a 8 bloques, se divide por 8 para saber el tag, ya que el
+  //tag de un set especifico aumenta cada 8 bloques (en este cache que tiene 8 sets)
+  return centered_block_number / NUMBER_OF_SETS;
+}
+
 
 void init() {
   memset(cache, 0, sizeof(set_t) * NUMBER_OF_SETS);
   memset(memory, 0, sizeof(char) * MEMORY_SIZE);
-  miss_rate = 0;
+  cache_accesses = 0;
+  number_of_misses = 0;
 }
 
-unsiged int get_offset(unsigned int address) {
-
+unsigned int get_offset(unsigned int address) {
+  return (address % BLOCK_SIZE);
 }
 
 unsigned int find_set(unsigned int address) {
-
+  unsigned int addressBlock = address / BLOCK_SIZE;
+  return (addressBlock % NUMBER_OF_SETS);
 }
 
 unsigned int select_oldest(unsigned int setnum) {
-  return oldest;
+  unsigned int oldest_index = 0;
+  for (size_t i = 0; i < ASSOCIATIVITY; ++i) {
+    if (cache[setnum].lines[i].times_used > cache[setnum].lines[oldest_index].times_used) {
+      oldest_index = i;
+    }
+  }
+  return oldest_index;
 }
 
 int compare_tag(unsigned int tag, unsigned int set) {
-  for (int i = 0; i < ASSOCIATIVITY; i++) {
-    if (cache[set].lines[i].tag == tag) return i;
+  for (int i = 0; i < ASSOCIATIVITY; ++i) {
+    if (cache[set].lines[i].is_valid && cache[set].lines[i].tag == tag) return i;
   }
   return -1;
 }
 
 void read_to_cache(unsigned int blocknum, unsigned int way, unsigned int set) {
-  char* block = memory[blocknum * BLOCK_SIZE];
-  memcpy(cache[set].lines[way].block, block, BLOCK_SIZE);
+  unsigned int address = blocknum * BLOCK_SIZE;
+  memcpy(cache[set].lines[way].block, memory + address, BLOCK_SIZE);
 }
 
 void write_to_cache(unsigned int address, unsigned char value) {
 
 }
 
-unsigned char read_byte(unsigned int address) {
+void _increase_blocks_use_count(unsigned int set) {
+  for (int i = 0; i < ASSOCIATIVITY; ++i) {
+    ++cache[set].lines[i].times_used;
+  }
+}
 
+unsigned char read_byte(unsigned int address) {
+  unsigned int set = find_set(address);
+  unsigned int offset = get_offset(address);
+  unsigned int tag = _get_tag(address);
+  int way = compare_tag(tag, set);
+  if (way == -1) {
+    way = select_oldest(set);
+    read_to_cache(_get_block_number(address), way, set);
+    ++number_of_misses;
+  }
+  _increase_blocks_use_count(set);
+  cache[set].lines[way].times_used = 0;
+  ++cache_accesses;
+  return cache[set].lines[way].block[offset];
 }
 
 void write_byte(unsigned int address, unsigned char value) {
+  memory[address] = value;//Escribo value en memoria
 
+  unsigned int set = find_set(address);
+  unsigned int offset = get_offset(address);
+  unsigned int tag = _get_tag(address);
+  int way = compare_tag(tag, set);
+  if (way != -1) {
+    _increase_blocks_use_count(set);
+    cache[set].lines[way].times_used = 0;
+    cache[set].lines[way].block[offset] = value;
+    ++cache_accesses;
+  }
 }
 
 float get_miss_rate() {
-  return miss_rate;
+  return ((float)number_of_misses)/((float)cache_accesses);
 }
 
+
 int main(int argc, char const *argv[]) {
-  /* code */
   return 0;
 }
